39c55596a2fe20774c4ae764d5f956e081166151
Christoph Dorn <christoph@christophdorn.com>
2015-10-13 23:27:18 -0700

    wip

--- for/virtual-dom/0-server.ccjson.js
+++ for/virtual-dom/0-server.ccjson.js
@@ -14,11 +14,18 @@ exports.forLib = function (LIB) {
                 self.AspectInstance = function (aspectConfig) {
 
                     var config = {};
-                    LIB._.merge(config, defaultConfig)
-                    LIB._.merge(config, instanceConfig)
-                    LIB._.merge(config, aspectConfig)
+                    LIB._.merge(config, defaultConfig);
+                    LIB._.merge(config, instanceConfig);
+                    LIB._.merge(config, aspectConfig);
 
-                    function transform (html) {
+                    function transform (html, callConfig) {
+
+                        var config = {};
+                        LIB._.merge(config, defaultConfig);
+                        LIB._.merge(config, instanceConfig);
+                        LIB._.merge(config, aspectConfig);
+                        LIB._.merge(config, callConfig);
+
                         return LIB.Promise.promisify(function (callback) {
 
                             html = html.replace(/^\s*|\s*$/g, "");
@@ -27,13 +34,15 @@ console.log("--------------------- html ---------------------");
 process.stdout.write(html);
 console.log("--------------------- html ---------------------");
 */
+
+                            var scriptLocations = {};
+                            scriptLocations[config.location || "window"] = true;
+
                             return CVDOM.html2hscript(html, {
                                 "controlAttributes": {
                                     "prefix": "data-component-",
                                     "remove": true,
-                                    "scriptLocations": {
-                                        "window": true
-                                    }
+                                    "scriptLocations": scriptLocations
                                 }
                             }, function(err, chscript, components, inlineScripts) {
                                 if (err) return callback(err);
@@ -77,7 +86,14 @@ console.log("--------------------- VDOM HSCRIPT ---------------------");
                                 code.push(  '  };');
                                 code.push(  '},');
                                 code.push(  'getScripts: function () {');
-                                code.push(    'return ' + JSON.stringify(inlineScripts) + ';');
+                                code.push('var scripts = {};');
+                                inlineScripts.forEach(function (inlineScript) {
+                                    code.push('if (!scripts["' + inlineScript.id + '"]) { scripts["' + inlineScript.id + '"] = {}; }');
+                                    code.push('scripts["' + inlineScript.id + '"]["' + inlineScript.location + '"] = function (exports) {');
+                                        code.push(inlineScript.code);
+                                    code.push('};');
+                                });
+                                code.push('return scripts;');
                                 code.push(  '}');
                                 if (config.format === "commonjs") {
                                     code.push('};');
@@ -86,7 +102,7 @@ console.log("--------------------- VDOM HSCRIPT ---------------------");
                                     code.push('});');
                                     code.push('\n</script>');
                                 }
-                                return callback(null, code.join(""));
+                                return callback(null, code.join("\n"));
                             });
                         })();
                     }
@@ -95,10 +111,10 @@ console.log("--------------------- VDOM HSCRIPT ---------------------");
                         transformer: function () {
                             return LIB.Promise.resolve(
                                 ccjson.makeDetachedFunction(
-                                    function (input) {
+                                    function (input, config) {
 
                                         return LIB.Promise.resolve(
-                                            transform(input)
+                                            transform(input, config)
                                         );
                                     }
                                 )
